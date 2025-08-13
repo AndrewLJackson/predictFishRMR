136f2de6cbb5320d80a1cec51f270ee831c0e1ce
Andrew Jackson <jacksoan@tcd.ie>
2025-08-13 16:43:42 +0100

    changed default parameter values in kfun()

--- R/kfun.R
+++ R/kfun.R
@@ -11,7 +11,7 @@
 #' kfun(1000)
 #' kfun(m = c(1, 10, 100, 1000), a = 0.0018, b = -0.63)
 kfun <-
-function(m, kpars = c(a = 0.0018, b = -0.63)){
+function(m, kpars = c(a = 0.00201, b = -0.617)){
   # Uses the reported allometric scaling of rate of cooling 
   # with body mass. Defaults to Nakamura values. 
   return( kpars[1] * m^(kpars[2]) )
