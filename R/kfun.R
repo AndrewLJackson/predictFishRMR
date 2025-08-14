fc3bee568d3914b2aa354f773d3afbcbde3c2497
Andrew Jackson <a.jackson@tcd.ie>
2025-08-14 15:19:54 +0100

    updated DESCRIPTION to remove unrequired pacakges

--- R/kfun.R
+++ R/kfun.R
@@ -14,7 +14,7 @@ kfun <-
 function(m, kpars = c(a = 0.00173, b = -0.6330)){
   
   # default values for regression excluding 
-  # basking sharks and whale sharks is  a = 0.00173, b = -0.6330.
+  # basking sharks and whale sharks is  a = 0.00173, b = -0.6330
   # defaults for including them is      a = 0.00201, b = -0.6166
   
   # if kpars is given as NULL then set to default
